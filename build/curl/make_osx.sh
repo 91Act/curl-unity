9c0cadc930753e469aba5876d751cd4f3dd5d584
jayatubi <yzk0370@gmail.com>
2019-02-27 15:12:26 +0800

    add prebuilt libs

--- build/curl/make_osx.sh
+++ build/curl/make_osx.sh
@@ -13,35 +13,35 @@ do_make()
     NGHTTP2_ROOT=$PROJ_ROOT/../nghttp2/prebuilt/osx
     export PKG_CONFIG_PATH=$NGHTTP2_ROOT:$PKG_CONFIG_PATH
     
-    # (
-    #     cd $CURL_VERSION
-    #     ./configure \
-    #         --host=i686-apple-darwin \
-    #         --prefix=$BUILD_DIR \
-    #         --with-ssl=$OPENSSL_ROOT \
-    #         -with-nghttp2 \
-    #         --enable-ipv6 \
-    #         --disable-ftp \
-    #         --disable-file \
-    #         --disable-ldap \
-    #         --disable-ldaps \
-    #         --disable-rtsp \
-    #         --disable-proxy \
-    #         --disable-dict \
-    #         --disable-telnet \
-    #         --disable-tftp \
-    #         --disable-pop3 \
-    #         --disable-imap \
-    #         --disable-smb \
-    #         --disable-smtp \
-    #         --disable-gopher \
-    #         --disable-manual \
-    #         --disable-shared
+    (
+        cd $CURL_VERSION
+        ./configure \
+            --host=i686-apple-darwin \
+            --prefix=$BUILD_DIR \
+            --with-ssl=$OPENSSL_ROOT \
+            -with-nghttp2 \
+            --enable-ipv6 \
+            --disable-ftp \
+            --disable-file \
+            --disable-ldap \
+            --disable-ldaps \
+            --disable-rtsp \
+            --disable-proxy \
+            --disable-dict \
+            --disable-telnet \
+            --disable-tftp \
+            --disable-pop3 \
+            --disable-imap \
+            --disable-smb \
+            --disable-smtp \
+            --disable-gopher \
+            --disable-manual \
+            --disable-shared
 
-    #     make clean
-    #     make -j8
-    #     make install
-    # )
+        make clean
+        make -j8
+        make install
+    )
 
     mkdir -p $PREBUILT_DIR
     cp -r $BUILD_DIR/* $PREBUILT_DIR
