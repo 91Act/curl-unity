a8b6b00e1c028491518217af2738b8d61c2f11d5
jayatubi <yzk0370@gmail.com>
2019-02-27 15:56:58 +0800

    nghttp2 for ios

--- build/nghttp2/make_osx.sh
+++ build/nghttp2/make_osx.sh
@@ -2,9 +2,11 @@ export NGHTTP2_ROOT=nghttp2-1.36.0
 export BUILD_DIR=`pwd`/build/osx
 export PREBUILT_DIR=`pwd`/prebuilt/osx
 
-mkdir -p $BUILD_DIR && cd $BUILD_DIR
-cmake ../../$NGHTTP2_ROOT -DENABLE_LIB_ONLY=ON -DENABLE_STATIC_LIB=ON -DENABLE_SHARED_LIB=OFF
-cd -
+mkdir -p $BUILD_DIR 
+(
+    cd $BUILD_DIR
+    cmake ../../$NGHTTP2_ROOT -DENABLE_LIB_ONLY=ON -DENABLE_STATIC_LIB=ON -DENABLE_SHARED_LIB=OFF
+)
 cmake --build $BUILD_DIR --config Release
 
 mkdir -p $PREBUILT_DIR/include
