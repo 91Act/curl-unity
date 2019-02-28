58b661ee54c5a985b6259eaf9edd69c5b21abd38
jayatubi <yzk0370@gmail.com>
2019-02-28 17:59:54 +0800

    unity wrapper

---
+++ Assets/curl-unity/Native/Interface.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace CurlUnity
+{
+    public static class Interface
+    {
+#if UNITY_IPHONE
+    public const string LIB_NAME = "__Internal";
+#else
+        public const string LIB_NAME = "curl";
+#endif
+
+        [DllImport(LIB_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern CURLcode curl_global_init(long flags);
+    }
+}
---
+++ Assets/curl-unity/Scripts/Curl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CurlUnity
+{
+    public class Curl : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var code = Interface.curl_global_init(0);
+            Debug.LogWarning(code);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
