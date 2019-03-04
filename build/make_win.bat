500ceb1a1f5c0015f3b6a57c6a20a15b1308ecf7
jayatubi <yzk0370@gmail.com>
2019-03-04 20:00:19 +0800

    Update windows build

--- build/make_win.bat
+++ build/make_win.bat
@@ -1,8 +1,8 @@
 for %%l in (openssl nghttp2 curl) do (
     pushd %%l
-    make_win.bat
+    call make_win.bat
     popd
     )
-
+    
 mkdir ..\Assets\curl-unity\Plugins\win
-cp build\curl\prebuilt\win\lib\libcurl.dll ..\Assets\curl-unity\Plugins\win\curl.dll
+cp curl\prebuilt\win\lib\libcurl.dll ..\Assets\curl-unity\Plugins\win\curl.dll
